dfc27225025695f682af85787247c755c359dd89
fcj <fabic@laptop.RandomBinaryThoughts.com>
2014-09-15 14:28:05 +0200

    FABIC: ./environment.sh : LD_RUN_PATH & al

--- environment.sh
+++ environment.sh
@@ -19,6 +19,12 @@ echo "+-"
 # Prefer our local/ stuff :
 if [ -x "$local_dir/bin/clang" ]; then
 	PATH="$local_dir/bin:$PATH"
+	# defining all of these, user will unset as needed :
+	LD_LIBRARY_PATH="$local_dir/lib"
+	LD_RUN_PATH="$local_dir/lib"
+	LIBRARY_PATH="$local_dir/lib"
+	CPATH="$local_dir/include"
+	CPLUS_INCLUDE_PATH="$local_dir/include/c++/v1"
 # over an eventual build/ :
 elif [ -x "$llvm_build_dir/bin/clang" ]; then
 	PATH="$llvm_build_dir/bin:$PATH"
@@ -34,7 +40,7 @@ elif [ ! -z "`which gcc 2> /dev/null`" ]; then
 fi
 
 
-Env=( CC CXX PATH LD_LIBRARY_PATH )
+Env=( CC CXX PATH LD_LIBRARY_PATH LD_RUN_PATH LIBRARY_PATH CPATH CPLUS_INCLUDE_PATH )
 
 export ${Env[*]}
 
