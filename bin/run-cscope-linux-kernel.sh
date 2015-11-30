72a8d35b2bce0f7b7c80a5cad0f410ea16ce6ea6
Fabien Cadet <fabi@fabic.net>
2015-11-30 18:09:04 +0100

    -_-

---
+++ bin/run-cscope-linux-kernel.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# @link http://cscope.sourceforge.net/large_projects.html
+#
+
+# LIST DIRS OF INTEREST :
+
+#    find -type d \( -path ./Documentation -o -path ./drivers -o -path ./firmware -o -path ./scripts -o -path ./sound \) -prune -o \( -path './arch/*' \! -path ./arch/x86 -prune \) -o -type d -print 
+
+# FILES for cscope.files :
+# find -type d \( -path ./Documentation -o -path ./drivers -o -path ./firmware -o -path ./scripts -o -path ./sound \) -prune -o \( -path './arch/*' \! -path ./arch/x86 -prune \) -o -type f -name "*.[chxsS]" > cscope.files
+
+
+# SIMPLER :
+# find arch/x86/ block/ crypto/ fs/ include/ init/ ipc/ kernel/ lib/ mm/ net/ethernet/ net/ipv4/ net/core/ net/dns_resolver/ net/netfilter/ net/packet/ net/unix/ tools/arch/x86/ -type f -name "*.[chxsS]" -print | sort > cscope.files
+
+LNX=$( cd `dirname "$0"`/../misc/linux-kernel/ && pwd )
+
+cd "$LNX" || exit 127
+
+# Note: using absolute paths in cscope.files for better compatibility (with editors).
+
+find "$LNX"                                                                   \
+	-path "$LNX/arch/*" ! -path "$LNX/arch/x86*" -prune -o               \
+	-path "$LNX/include/asm-*" ! -path "$LNX/include/asm-i386*" -prune -o \
+	-path "$LNX/tmp*" -prune -o                                           \
+	-path "$LNX/Documentation*" -prune -o                                 \
+	-path "$LNX/scripts*" -prune -o                                       \
+	-path "$LNX/drivers*" -prune -o                                       \
+	-name "*.[chxsS]" -print                                              \
+		| sort                                                        \
+		| tee "$LNX/cscope.files"
+
+retv=$?
+
+[ $retv -eq 0 ] || exit 126
+
+
+exit
+
+
+time \
+	cscope -b -q -k
+
+retv=$?
+
+echo "Cscope exit status : $retv"
+
+exit $retv
+# FIXME: vim: set et ts=4 sts=4 sw=4
