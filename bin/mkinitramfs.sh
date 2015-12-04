6801a32391835c625e13171b52b79d1ea844efb9
Fabien Cadet <cadet@pc126.pallas.cines.fr>
2015-12-04 12:27:05 +0100

    New bin/mkinitramfs.sh  (empty, wasn't used)

---
+++ bin/mkinitramfs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+# This file based in part on the mkinitrafms script for the LFS LiveCD
+# written by Alexander E. Patrakov and Jeremy Huntwork.
+#
+# FC.2015-12-03 : This is borrowed from LFS.
+#
+# @link http://www.linuxfromscratch.org/blfs/view/svn/postlfs/initramfs.html
+#
+# See also @link https://wiki.gentoo.org/wiki/Custom_Initramfs
+
+# EDIT: REMOVED CONTENT (ended up not using this script)
