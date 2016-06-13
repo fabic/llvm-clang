c8c197829b5f84fbd869f376cd68e6061f68ec9d
Fabien Cadet <fabi@fabic.net>
2016-06-13 18:23:49 +0200

    not much while killin' time

--- gcc_environment.sh
+++ gcc_environment.sh
@@ -9,16 +9,29 @@ CXX=g++
 export CC CXX
 
 BOOST_ROOT="$HOME/boost-1.61.0-gcc" 
-export BOOST_ROOT
 
-#BOOST_INCLUDE_DIRS="${BOOST_ROOT}/include"
-#BOOST_LIBRARY_DIRS="${BOOST_ROOT}/lib"
-#export BOOST_INCLUDE_DIRS BOOST_LIBRARY_DIRS
+if [ -d "$BOOST_ROOT/include" ];
+then
+	echo "FYI: Found Boost @ $BOOST_ROOT"
 
-#INCLUDE_PATH="$BOOST_ROOT/include:$INCLUDE_PATH"
-#export INCLUDE_PATH
+	export BOOST_ROOT
 
-fhs_path_setup_for "$BOOST_ROOT"
+	BOOST_INCLUDE_DIRS="${BOOST_ROOT}/include"
+	BOOST_LIBRARY_DIRS="${BOOST_ROOT}/lib"
+	export BOOST_INCLUDE_DIRS BOOST_LIBRARY_DIRS
 
-export CPATH LIBRARY_PATH
+	pathprepend "$BOOST_INCLUDE_DIRS" CPLUS_INCLUDE_PATH
+	pathprepend "$BOOST_LIBRARY_DIRS" LD_RUN_PATH
+
+
+	#INCLUDE_PATH="$BOOST_ROOT/include:$INCLUDE_PATH"
+	#export INCLUDE_PATH
+
+	#fhs_path_setup_for "$BOOST_ROOT"
+
+	export CPLUS_INCLUDE_PATH LD_RUN_PATH
+else
+    echo "FYI: couldn't find Boost @ \$BOOST_ROOT=$BOOST_ROOT  (unsetting env. var.)."
+    unset BOOST_ROOT
+fi
 
--- misc/install_boost_cpp.sh
+++ misc/install_boost_cpp.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+cd misc/boost
+
+git describe
+
 ./bootstrap.sh --prefix="${HOME}/$(git describe --abbrev=0)-${CC}" --with-toolset=${CC}
 
-
 time ./b2 --stagedir=. --layout=tagged variant=debug,release link=shared threading=multi address-model=64 cxxflags='-std=c++14' install      
+
+##
+
+cd misc/cpp-netlib/
+
+git describe
+
+mkcd build
+
+# Note that having the env. var. BOOST_ROOT won't do it, we have to pass the arg. -DBOOST_ROOT=... (unfortunately).
+# Note: see also their build.sh shell script.
+cmake -DCMAKE_BUILD_TYPE=Debug -DBOOST_ROOT=$BOOST_ROOT ..
 
