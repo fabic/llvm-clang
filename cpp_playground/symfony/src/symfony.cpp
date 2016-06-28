f1c28310c437234e617a3f426bff681ce7775193
Fabien Cadet <fabi@fabic.net>
2016-06-28 17:57:11 +0200

    code reformat

--- cpp_playground/symfony/src/symfony.cpp
+++ cpp_playground/symfony/src/symfony.cpp
@@ -9,15 +9,12 @@
 #include "fabic/di/container.hpp"
 #include "fabic/di/dll/dll_service_provider.hpp"
 
-
-
 namespace fabic {
-namespace symfony {
+  namespace symfony {
 
     namespace po   = boost::program_options;
 
     using program_arguments = po::variables_map;
-
 
     /**
      * MAIN secondary entry point
@@ -26,98 +23,86 @@ namespace symfony {
      */
     int main(program_arguments &args)
     {
-        loginfo << "MAIN: Hello world, you reached this point, can't believe it!";
-        loginfo << "  » address : " << args["address"].as<std::string>();
-        loginfo << "  » port : "    << args["port"].as<int>();
-        loginfo << "";
+      loginfo << "MAIN: Hello world, you reached this point, can't believe it!";
+      loginfo << "  » address : " << args["address"].as<std::string>();
+      loginfo << "  » port : " << args["port"].as<int>();
+      loginfo << "";
 
-        namespace di = fabic::di;
+      namespace di = fabic::di;
 
-        auto cnt = di::service_container::new_container_instance();
+      auto cnt = di::service_container::new_container_instance();
 
+      // DLL
 
-        // DLL
+      auto dll = std::make_shared<di::dll::dll_service_provider>(cnt);
 
-        auto dll = std::make_shared<di::dll::dll_service_provider>(cnt);
+      dll->load_library("build/symfony/module/http/libmodule-http-server.so");
 
-        dll->load_library( "build/symfony/module/http/libmodule-http-server.so" );
+      cnt->debugDumpContainer(std::cout);
 
-        cnt->debugDumpContainer(std::cout);
+      //auto huh = cnt->get_service<server>("http.server");
 
-
-        //auto huh = cnt->get_service<server>("http.server");
-
-
-        return 0;
+      return 0;
     }
-
 
     /**
      * Resort to Boost's program_options lib. for parsing command line arguments.
      */
     program_arguments
     process_program_arguments(int argc, const char *const argv[]) {
-        namespace po = boost::program_options;
+      namespace po = boost::program_options;
 
-        // This is needed so for setting up the
-        // “ conversion facet according to the user's selected locale ”
-        // based on the LANG environment variable. Else an exception is
-        // thrown from some deep abyss "character conversion failed".
-        //   http://www.boost.org/doc/libs/1_61_0/doc/html/program_options/howto.html#idp308994400
-        //   http://en.cppreference.com/w/cpp/locale/locale/global
-        std::locale::global(std::locale(""));
-        //std::locale::global(std::locale("en_US.UTF-8"));
-        //std::locale::global(std::locale::classic());
+      // This is needed so for setting up the
+      // “ conversion facet according to the user's selected locale ”
+      // based on the LANG environment variable. Else an exception is
+      // thrown from some deep abyss "character conversion failed".
+      //   http://www.boost.org/doc/libs/1_61_0/doc/html/program_options/howto.html#idp308994400
+      //   http://en.cppreference.com/w/cpp/locale/locale/global
+      std::locale::global(std::locale(""));
+      //std::locale::global(std::locale("en_US.UTF-8"));
+      //std::locale::global(std::locale::classic());
 
-        po::options_description desc("Allowed options");
-        desc.add_options()
-                ("help", "produce help message")
-                ("address",
-                    po::value<std::string>()->default_value("localhost"),
-                    "Hostname or IP address")
-                ("port",
-                    po::value<int>()->default_value(1234),
-                    "TCP port number");
+      po::options_description desc("Allowed options");
+      desc.add_options()
+          ("help", "produce help message")
+          ("address",
+           po::value<std::string>()->default_value("localhost"),
+           "Hostname or IP address")
+          ("port",
+           po::value<int>()->default_value(1234),
+           "TCP port number");
 
-        po::positional_options_description posit;
-        posit.add("extra", -1);
+      po::positional_options_description posit;
+      posit.add("extra", -1);
 
-        po::variables_map args;
+      po::variables_map args;
 
-        auto parser = po::command_line_parser(argc, argv);
+      auto parser = po::command_line_parser(argc, argv);
 
-        parser
-                .options(desc)
-                .positional(posit);
+      parser
+          .options(desc)
+          .positional(posit);
 
-        auto parsed_options = parser.run();
+      auto parsed_options = parser.run();
 
-        po::store(parsed_options, args, true);
+      po::store(parsed_options, args, true);
 
-        po::notify(args);
+      po::notify(args);
 
-        return args;
+      return args;
     }
 
-} // symfony ns.
+  } // symfony ns.
 } // fabic ns
 
 
 /**
- * MAIN !
+ * MAIN (the libc) !
  */
-int main(int argc, const char *argv[])
-{
-    try {
-        auto args = fabic::symfony::process_program_arguments(argc, argv);
-        auto exit_status = fabic::symfony::main(args);
-        return exit_status;
-    }
-    catch (std::exception& e) {
-        std::cerr << "OUPS! Caught sthg: " << e.what() << std::endl;
-        std::cerr << "      (forwarding exception)" << std::endl;
-        throw e;
-    }
+int main(int argc, const char *argv[]) {
+  auto args = fabic::symfony::process_program_arguments(argc, argv);
 
-    return 127;
+  auto exit_status = fabic::symfony::main(args);
+
+  return exit_status;
 }
