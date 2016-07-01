c2c80bdc0aa558ba8d66da64e1ed84bc6dab0dd9
Fabien Cadet <fabi@fabic.net>
2016-07-01 16:35:36 +0200

    check-clang-include-paths.sh

--- check-clang-include-paths.sh
+++ check-clang-include-paths.sh
@@ -1,4 +1,32 @@
 #!/bin/sh
+#
+# FabiC/2016-06-20 -- https://github.com/fabic/llvm-clang
 
-echo | clang++ -Wp,-v -x c++ - -fsyntax-only
+self="${BASH_SOURCE[0]}"
 
+echo "+- $self $@"
+echo "|"
+echo "| This shell script basically invokes Clang++ as :"
+echo "|"
+echo "|     clang++ -Wp,-v -x c++ - -fsyntax-only < /dev/null"
+echo "|"
+echo "| FYI: Note that the following command would give you more verbose details :"
+echo "|"
+echo "|     clang++ -E -x c++ - -v < /dev/null"
+echo "|"
+echo "| Additionnal command line arguments are passed to Clang++, ex. :"
+echo "|"
+echo "|     $self -nostdinc -nostdinc++"
+echo "|     $self -stdlib=libc++"
+echo "|     $self -stdlib=libstdc++"
+echo "|"
+echo "+- Output :"
+echo "|"
+
+clang++ -Wp,-v -x c++ - -fsyntax-only "$@" < /dev/null |&
+  sed -e 's/^/|  /'
+
+retv=$?
+
+echo "|"
+echo "+- $self $@"
