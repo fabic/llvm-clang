e2d2b5f92aca55d50c6a10aa2c9bc18053c3a45c
fcj <fabic@laptop.RandomBinaryThoughts.com>
2016-07-03 16:40:09 +0200

    Damn!

--- cpp_playground/clang-check/manual-build.sh
+++ cpp_playground/clang-check/manual-build.sh
@@ -50,3 +50,96 @@ clang++  \
       -lclangTooling  \
         -o clang-check  \
           ClangCheck.cpp
+
+      # from the `ldd ...` output of the original clang-check binary :
+      # -lLLVMAnalysis \
+      # -lLLVMAsmPrinter \
+      # -lLLVMBitReader \
+      # -lLLVMBitWriter \
+      # -lLLVMCodeGen \
+      # -lLLVMCore \
+      # -lLLVMInstCombine \
+      # -lLLVMInstrumentation \
+      # -lLLVMMC \
+      # -lLLVMMCDisassembler \
+      # -lLLVMMCParser \
+      # -lLLVMObject \
+      # -lLLVMOption \
+      # -lLLVMProfileData \
+      # -lLLVMScalarOpts \
+      # -lLLVMSelectionDAG \
+      # -lLLVMSupport \
+      # -lLLVMTarget \
+      # -lLLVMTransformUtils \
+      # -lLLVMX86AsmParser  \
+      # -lLLVMX86AsmPrinter  \
+      # -lLLVMX86CodeGen  \
+      # -lLLVMX86Desc  \
+      # -lLLVMX86Disassembler  \
+      # -lLLVMX86Info  \
+      # -lLLVMX86Utils \
+      # -lclangAST  \
+      # -lclangASTMatchers \
+      # -lclangAnalysis \
+      # -lclangBasic  \
+      # -lclangDriver  \
+      # -lclangEdit \
+      # -lclangFrontend  \
+      # -lclangLex \
+      # -lclangParse \
+      # -lclangRewrite \
+      # -lclangRewriteFrontend  \
+      # -lclangSema \
+      # -lclangSerialization \
+      # -lclangStaticAnalyzerCheckers \
+      # -lclangStaticAnalyzerCore \
+      # -lclangStaticAnalyzerFrontend  \
+      # -lclangTooling \
+      # -lclangToolingCore  \
+
+
+      # Tried with absolute paths :
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMAnalysis.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMAsmPrinter.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMBitReader.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMBitWriter.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMCodeGen.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMCore.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMInstCombine.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMInstrumentation.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMMC.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMMCDisassembler.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMMCParser.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMObject.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMOption.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMProfileData.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMScalarOpts.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMSelectionDAG.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMSupport.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMTarget.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMTransformUtils.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86AsmParser.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86AsmPrinter.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86CodeGen.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86Desc.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86Disassembler.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86Info.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libLLVMX86Utils.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangAST.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangASTMatchers.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangAnalysis.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangBasic.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangDriver.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangEdit.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangFrontend.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangLex.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangParse.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangRewrite.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangRewriteFrontend.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangSema.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangSerialization.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangStaticAnalyzerCheckers.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangStaticAnalyzerCore.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangStaticAnalyzerFrontend.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangTooling.so \
+      # /media/g/home/fabi/dev/llvm/local/lib/libclangToolingCore.so \
