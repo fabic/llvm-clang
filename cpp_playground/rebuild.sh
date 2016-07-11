4cd263c33481f5f0f6f28e5e072164faa8846fdb
Fabien Cadet <fabi@fabic.net>
2016-07-11 17:13:46 +0200

    cpp_playground rebuild.sh

---
+++ cpp_playground/rebuild.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+here=$(cd dirname "$0" && pwd)
+
+cd "$here" || exit 1
+
+builddir="build"
+
+[ ! -z "$builddir" ] || exit 2
+
+if [ -d "$builddir" ];
+then
+  echo "| REMOVING build dir. '$builddir'"
+  rm -rf "$builddir" || exit 3
+fi
+
+[ -x build.sh ] || exit 4
+
+./build.sh "$@"
+
+retv=$?
+
+if [ $retv -gt 0 ];
+then
+  echo
+  echo "+~> "$0 $@" : FAILED, build.sh exited w/ status $retv"
+  echo
+  exit $retv
+fi
+
