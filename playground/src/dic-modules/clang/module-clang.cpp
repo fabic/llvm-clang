db1b2192cbf4b4a46c6e38f427998e475d15d029
Fabien Cadet <fabi@winterfell.home>
2017-03-07 19:38:39 +0400

    Good bye playground/ ...

--- playground/src/dic-modules/clang/module-clang.cpp
+++
@@ -1,7 +0,0 @@
-/**
- * \brief /me wandering LLVM/Clang's internals...
- *
- * References :
- *   - http://clang.llvm.org/docs/Tooling.html#libtooling
- *   - ^ http://clang.llvm.org/docs/LibTooling.html
- */
