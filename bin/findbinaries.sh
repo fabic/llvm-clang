5d4d8e603e1d7d6c8e3644f0afd03ae37d1af98a
Fabien Cadet <fabi@winterfell.fabic.net>
2017-12-19 23:55:39 +0400

    bin/findbinaries.sh : made better...

--- bin/findbinaries.sh
+++ bin/findbinaries.sh
@@ -3,11 +3,36 @@
 
 echo "+--- $0 $@"
 echo "| In '`pwd`'"
-echo "|"
 
-find \( -type d \( -name CMakeFiles -o -name .git -o -name .svn \) -prune \) \
-    -o -type f -perm -a+x \
-    "$@" \
-    -print0 | \
-        xargs -0r ls -ltrh | \
-            sed -e 's@^@|  &@'
+# Note about command line arguments (if none provided).
+if [ $# -lt 1 ]; then
+  echo "+"
+  echo "| Note that additional arguments may be provided, these would be passed"
+  echo "| to the \`find ...\` command."
+fi
+
+find_cmd=(
+  find \( -type d \( -name CMakeFiles -o -name '.?*' \) -prune \)
+    -o \( -type f \( -perm -a+x -o -name '*.a' \) \)
+    "$@"
+)
+
+# Build a string for printing out the `find ...` command we are about to
+# run, with Bash special characters ()* escaped ;
+# (so that one may copy-paste it and just work).
+escaped_cmd_str="${find_cmd[@]}"
+escaped_cmd_str="${escaped_cmd_str//\(/\\(}"
+escaped_cmd_str="${escaped_cmd_str//\)/\\)}"
+escaped_cmd_str="${escaped_cmd_str//\*/\\\*}"
+
+echo "+"
+echo "| Find command :"
+echo "|"
+echo "|   $escaped_cmd_str"
+echo "+"
+
+"${find_cmd[@]}" \
+  -print0 | \
+    xargs -0r ls -ltrh | \
+      sed -e 's@^@|  &@'
+
